981b8c3dd1e56a14761139f3fd16d5020de47d29
Fazley <22FTT1494@student.pb.edu.bn>
2024-03-30 17:35:00 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AS3 Assignment 2 MWE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
